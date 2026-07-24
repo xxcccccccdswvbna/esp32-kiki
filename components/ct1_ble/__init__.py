9b30636d652188f7952c552b9af61a7283285209
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-24 08:57:41 +0800

    Update __init__.py

--- components/ct1_ble/__init__.py
+++ components/ct1_ble/__init__.py
@@ -1,13 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import mqtt
 from esphome.const import CONF_ID
 
-# 定义命名空间
 ct1_ble_ns = cg.esphome_ns.namespace("ct1_ble")
-CT1BLE = ct1_ble_ns.class_("CT1BLE", cg.Component, mqtt.MQTTDevice)
-
-CONF_CT1_BLE_ID = "id"
+CT1BLE = ct1_ble_ns.class_("CT1BLE", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CT1BLE),
